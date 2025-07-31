2763af388440d5a746b2132e7afa044ee2f70044
jitendra784 <jitendravenom784@gmail.com>
2025-07-31 17:53:24 +0530

    Fourth

--- back_end/models/addDsrmodel.js
+++ back_end/models/addDsrmodel.js
@@ -1,35 +1,13 @@
 const mongoose = require("mongoose");
 
 const dsrSchema = new mongoose.Schema({
- email: {
-  type: String,
-  required: true,
-  },
-  date:  {
-  type : Date,
-  required: true,
-  },
-attachment:{
-  type: String,
-  required : true,
-  },
-  projectName:
-  { 
-    type: String,
-    required: true,
-  },
-  
-  projectDescription:
-  {
-  type :  String,
-  required: true,
-},
-  todoTasks: 
-  {
-  type: String,
-  required: true,
-  }
+  email: String,
+  date: String,
+  attachment: String,
+  projectName: String,
+  projectDescription: String,
+  todoTasks: [String]
 }, { timestamps: true });
 
-module.exports = mongoose.model("AddDsr", dsrSchema);
+module.exports = mongoose.model("dsr", dsrSchema);
 
