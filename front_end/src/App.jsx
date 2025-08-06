6c426bb78504cd22f4348917d8c0d0303a0bc9c2
shubham <shubhambadaya009@gmail.com>
2025-08-06 22:24:29 +0530

    worked on role based authanticate dashboard

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -14,13 +14,12 @@ import Sign_in from "./auth/sign_in";
 import ProfileForm from "./pages/user/profile/profile";
 import UserPage from "./pages/user/userPage";
 import Layout from "./layout/layout";
-import Dashboard from "./dashboard/dashboard";
+import Dashboard from "./dashboard/Dashboard"; // updated import
 import Profile_info from "./pages/user/profile/profile_infos";
 import DSR_List from "./pages/dsrListReport/DSR_List";
 import Add_Dsr_Report from "./pages/dsrListReport/Add_Dsr_Report";
 import AttendanceSheet from "./pages/attendanceSheet/AttendanceSheet";
 import Daily_Attendance from "./pages/attendanceSheet/Daily_Attendance";
-import "react-toastify/dist/ReactToastify.css";
 import HolidayCalender from "./pages/holidayCalender/HolidayCalender";
 import AddEmpReference from "./pages/empReference/AddEmpReference ";
 import EmpReferenceListing from "./pages/empReference/EmpReferenceListing";
@@ -28,7 +27,6 @@ import AddResign from "./pages/resign/AddResign";
 import ResignList from "./pages/resign/ResignList";
 import AddLeave from "./pages/leave/AddLeave";
 import LeaveList from "./pages/leave/LeaveList";
-
 import AddUserPage from "./pages/user/AddUserPage";
 import AddUserForm from "./pages/user/AddUserForm";
 import Role from "./pages/user/role";
@@ -63,33 +61,13 @@ function App() {
                     <Route path="/users" element={<UserPage />} />
                     <Route path="/add-user" element={<AddUserForm />} />
                     <Route path="/user/:id?" element={<AddUserPage />} />
-                    <Route path="/add-user" element={<AddUserPage />} />
-                    <Route path="/add-user/:id" element={<AddUserPage />} />
-                    <Route
-                      path="/holidayCalender"
-                      element={<HolidayCalender />}
-                    />
+                    <Route path="/holidayCalender" element={<HolidayCalender />} />
                     <Route path="/dsr_list" element={<DSR_List />} />
-                    <Route
-                      path="/add_Dsr_Report"
-                      element={<Add_Dsr_Report />}
-                    />
-                    <Route
-                      path="/attendanceSheet"
-                      element={<AttendanceSheet />}
-                    />
-                    <Route
-                      path="/daily_Attendance"
-                      element={<Daily_Attendance />}
-                    />
-                    <Route
-                      path="/addEmpReference"
-                      element={<AddEmpReference />}
-                    />
-                    <Route
-                      path="/empReferenceListing"
-                      element={<EmpReferenceListing />}
-                    />
+                    <Route path="/add_Dsr_Report" element={<Add_Dsr_Report />} />
+                    <Route path="/attendanceSheet" element={<AttendanceSheet />} />
+                    <Route path="/daily_Attendance" element={<Daily_Attendance />} />
+                    <Route path="/addEmpReference" element={<AddEmpReference />} />
+                    <Route path="/empReferenceListing" element={<EmpReferenceListing />} />
                     <Route path="/addResign" element={<AddResign />} />
                     <Route path="/resignList" element={<ResignList />} />
                     <Route path="/info" element={<Profile_info />} />
