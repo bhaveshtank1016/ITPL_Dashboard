cf62b813cff856313ff1b059b1b0785bfb466979
Bhavesh162002 <bhomik.kumar16@gmail.com>
2025-08-06 17:29:31 +0530

    test

--- back_end/middleware/authMiddleware.js
+++ back_end/middleware/authMiddleware.js
@@ -9,6 +9,7 @@ const protect = async (req, res, next) => {
     req.headers.authorization.startsWith("Bearer")
   ) {
     try {
+      // check
       token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
